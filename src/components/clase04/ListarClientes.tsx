b479ab80901e62b09cf4f22142ad3a79b9b30afa
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-07-22 18:44:20 -0500

    Update ListarClientes.tsx

--- src/components/clase04/ListarClientes.tsx
+++ src/components/clase04/ListarClientes.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import axios from 'axios'
 import { Table, Button } from 'react-bootstrap'
+import { Cliente } from './model/cliente'
 
 type Props = {}
 
 const ListarClientes = (props: Props) => {
-    
+  const [token, setToken] = useState<string>("");
+  const [lista, setLista] = useState<Cliente[]>([]);
+
+  const cargarDatos = useCallback(()=>{
+    setToken("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbm9uaW1vQGdtYWlsLmNvbSIsImlhdCI6MTcyMTY5MDI4OSwiZXhwIjoxNzIxNjkxNDg5fQ.tTfjps3Fe451EEktFrzYevrzl9Qt7SHGiYIEbf50qmw");
+    const config = {            
+      headers: { Authorization: `Bearer ${ token }` }
+    }
+
+    axios.get('http://localhost:8089/v1/cliente/listarClientes', config)
+      .then((res)=> {        
+        setLista(res.data);
+      })
+      .catch((error) => {console.error(error)})
+      .finally(()=>{console.log("TERMINADO")});
+  }, [token]);
+
+  
 
 
   return (
-    <Table>
+    <>
+      <Button onClick={cargarDatos}  variant="primary" className="w-100">Cargar Datos</Button>
+      <Table>
         <thead>
             <tr>
                 <th>ID</th>
@@ -17,10 +38,27 @@ const ListarClientes = (props: Props) => {
                 <th>Telefono</th>
                 <th>Email</th>
                 <th>fecha Registro</th>
-                <th>Acciones</th>
-            </tr>
+                <th>Acciones</th>                
+            </tr>            
+            {
+              lista.map((dato, indice) => (
+                <tr>
+                  <td>{dato.clienteId}</td>                    
+                  <td>{dato.nombre}</td>                    
+                  <td>{dato.direccion}</td>                    
+                  <td>{dato.telefono}</td>                    
+                  <td>{dato.email}</td>     
+                  <td>{dato.fechaRegistro.toString()}</td>                                      
+                  <td>
+                    <Button variant="primary">M</Button>{ ' '}
+                    <Button variant="danger">E</Button>
+                  </td>
+                </tr>
+              ))
+            };
         </thead>
-    </Table>
+      </Table>
+    </>    
   )
 }
 
