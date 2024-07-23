4304269fb85f43b907d6c75a023307c773670ad5
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-07-22 19:19:47 -0500

    Update ModalConsulta.tsx

--- src/components/clase04/ModalConsulta.tsx
+++ src/components/clase04/ModalConsulta.tsx
@@ -1,1 +1,73 @@
 
+import { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+type Props = {}
+
+const ModalConsulta = (props: Props) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Nuevo Cliente
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Nuevo Cliente</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+          <Form.Group controlId="Nombre">
+                            <Form.Label>Nombre</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Nombre"
+                                
+                            />
+            </Form.Group>
+            <Form.Group controlId="Direccion">
+                            <Form.Label>Dirección</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Dirección"
+                                
+                            />
+            </Form.Group>
+            <Form.Group controlId="Telefono">
+                            <Form.Label>Telefono</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Telefono"
+                                
+                            />
+            </Form.Group>
+            <Form.Group controlId="Email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Email"
+                                
+                            />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default ModalConsulta;
