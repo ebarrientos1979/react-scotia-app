27cb2d2e4a4e03fde6534f7aebf4e28435c6f50f
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-07-24 17:16:20 -0500

    Modificacion

--- src/components/clase04/ModalModif.tsx
+++ src/components/clase04/ModalModif.tsx
@@ -3,6 +3,8 @@ import { Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Cliente } from './model/cliente';
+import axios from 'axios';
+import ListarClientes from './ListarClientes';
 
 type Props = {
     cliente: Cliente, 
@@ -13,16 +15,45 @@ type Props = {
 const ModalModif = (props: Props) => {
   const [show, setShow] = useState(false);
   const [nombre, setNombre] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [email, setEmail] = useState("");
   const handleClose = () => setShow(false);
+
+  const saveCliente = () => {
+    const fechaActual = new Date();
+    const cliente:Cliente = {
+      clienteId: props.cliente.clienteId,
+      nombre: nombre,
+      direccion: direccion,
+      telefono: telefono,
+      email: email,
+      fechaRegistro: fechaActual,
+      cuentas: props.cliente.cuentas,
+    }
+    
+    
+    axios.post("http://localhost:8089/v1/cliente/save", cliente,
+      {            
+        headers: { Authorization: `Bearer ${ localStorage.getItem("tokenAcces") }` }
+      })
+      .then(
+        res => console.log(res)
+      );
+  };
 
   useEffect(() => {
     if (props.indice == props.indiceSel){
         setShow(true)
         setNombre(props.cliente.nombre)
+        setDireccion(props.cliente.direccion)
+        setTelefono(props.cliente.telefono)
+        setEmail(props.cliente.email)
     }
     //props.flagShow
   }, []);
 
+  
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -37,6 +68,7 @@ const ModalModif = (props: Props) => {
                                 type="text"
                                 placeholder="Nombre"
                                 value={nombre}
+                                onChange={ (evento) => setNombre(evento.target.value) }
                             />
             </Form.Group>
             <Form.Group controlId="Direccion">
@@ -44,7 +76,8 @@ const ModalModif = (props: Props) => {
                             <Form.Control
                                 type="text"
                                 placeholder="Dirección"
-                                
+                                value={direccion}
+                                onChange={ (evento) => setDireccion(evento.target.value) }
                             />
             </Form.Group>
             <Form.Group controlId="Telefono">
@@ -52,7 +85,8 @@ const ModalModif = (props: Props) => {
                             <Form.Control
                                 type="text"
                                 placeholder="Telefono"
-                                
+                                value={telefono}
+                                onChange={ (evento) => setTelefono(evento.target.value) }
                             />
             </Form.Group>
             <Form.Group controlId="Email">
@@ -60,17 +94,18 @@ const ModalModif = (props: Props) => {
                             <Form.Control
                                 type="text"
                                 placeholder="Email"
-                                
+                                value={email}
+                                onChange={ (evento) => setEmail(evento.target.value) }
                             />
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
-            Close
+            Cerrar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="primary" onClick={saveCliente}>
+            Grabar
           </Button>
         </Modal.Footer>
       </Modal>
